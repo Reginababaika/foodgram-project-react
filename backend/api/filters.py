a0fad6d2416505336667648252d6b196d90094ec
Reginababaika <gins15@mail.ru>
2023-07-30 09:56:26 +0300

    done

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -7,7 +7,8 @@ class RecipeFilter(FilterSet):
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='get_is_in_shopping_cart')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
         return queryset.filter(
